ab03a5bf8a4e5576c1ec9f7f36e5b6e82bf7ebd9
Fatima Ali <fatimaaliforwork@gmail.com>
2023-08-28 13:31:50 +0500

    setsup routes in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,40 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Dashboard from "pages/dashboard";
+import Layout from "pages/layout";
+
 function App() {
-	return <div className="App">Hello</div>;
+	const mode = useSelector((state) => state.global.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route element={<Layout />}>
+							<Route
+								path="/"
+								element={
+									<Navigate
+										to="/dashboard"
+										replace
+									/>
+								}
+							/>
+							<Route
+								path="/dashboard"
+								element={<Dashboard />}
+							/>
+						</Route>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
