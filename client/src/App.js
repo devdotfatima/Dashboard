757ced7528ed2e85bc693b79a1e297112c82b772
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-04 17:15:55 +0500

    Integrates react query

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,41 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Dashboard from "pages/dashboard";
 import Layout from "pages/layout";
 
+const queryClient = new QueryClient();
 function App() {
 	const mode = useSelector((state) => state.global.mode);
 	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 	return (
-		<div className="App">
-			<BrowserRouter>
-				<ThemeProvider theme={theme}>
-					<CssBaseline />
-					<Routes>
-						<Route element={<Layout />}>
-							<Route
-								path="/"
-								element={
-									<Navigate
-										to="/dashboard"
-										replace
-									/>
-								}
-							/>
-							<Route
-								path="/dashboard"
-								element={<Dashboard />}
-							/>
-						</Route>
-					</Routes>
-				</ThemeProvider>
-			</BrowserRouter>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className="App">
+				<BrowserRouter>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<Routes>
+							<Route element={<Layout />}>
+								<Route
+									path="/"
+									element={
+										<Navigate
+											to="/dashboard"
+											replace
+										/>
+									}
+								/>
+								<Route
+									path="/dashboard"
+									element={<Dashboard />}
+								/>
+							</Route>
+						</Routes>
+					</ThemeProvider>
+				</BrowserRouter>
+			</div>
+		</QueryClientProvider>
 	);
 }
 
