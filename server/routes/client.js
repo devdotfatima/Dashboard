6046cc68a278458ecee460bb84a5275d75891fc1
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-07 20:20:56 +0500

    Adds route to get locations of users in BE

--- server/routes/client.js
+++ server/routes/client.js
@@ -10,5 +10,6 @@ const router = express.Router();
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 export default router;
